1e950c0ce30b64c0706652d290583ae8030fcec0
jaremko <w3techplayground@gmail.com>
2024-01-31 16:30:52 +0100

    WIP

--- src/tvt/a7/profedit/util.clj
+++ src/tvt/a7/profedit/util.clj
@@ -28,3 +28,26 @@
   (sc/invoke-later
    (sc/config! frame :on-close :nothing)
    (sc/dispose! frame)))
+
+
+(defn mean [vals]
+  (/ (reduce + vals) (count vals)))
+
+
+(defn linear-regression-coefficients [data]
+  (let [x-mean (mean (map :temperature data))
+        y-mean (mean (map :speed data))
+        numerator (reduce + (map (fn [{:keys [temperature speed]}]
+                                   (* (- temperature x-mean) (- speed y-mean)))
+                               data))
+        denominator (reduce + (map (fn [{:keys [temperature]}]
+                                     (Math/pow (- temperature x-mean) 2))
+                               data))]
+    {:m (/ numerator denominator)
+     :b (- y-mean (* (/ numerator denominator) x-mean))}))
+
+
+(defn calculate-percent-change-linear-regression [data]
+  (let [{:keys [m]} (linear-regression-coefficients data)
+        avg-speed (mean (map :speed data))]
+    (* 1500 (/ m avg-speed))))
